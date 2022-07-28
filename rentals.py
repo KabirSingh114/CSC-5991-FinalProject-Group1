e8b8e6bd2849b32d93544e4f3c3b4eadc21d9485
Hilda Qardagh <hf9574@wayne.edu>
2022-07-28 18:00:54 -0400

    New code

---
+++ rentals.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, render_template, session, abort, flash
+import os
+import pymongo
+from datetime import datetime
+import json
+from bson.json_util import dumps, loads
+
+app = Flask(__name__)
+
+
+@app.route('/rent', methods=['POST'])
+def rental():
+
+    mongoClient = pymongo.MongoClient('mongodb://localhost:27017/')
+    mongoDb = mongoClient['w_books']
+    bookCollection = mongoDb['books_rentals']
+    result = bookCollection.insert_one({
+        'book_id': request.form['book_id'],
+        'renter_name': request.form['renter_name'],
+        'rental_date': datetime.now(),
+        'is_returned': 0,
+        'return_date': 0
+    }).inserted_id
+
+    rentalRecord = bookCollection.find_one(result)
+    return dumps(rentalRecord)
+
+
+if __name__ == '__main__':
+    app.secret_key = os.urandom(12)
+    app.run(host='0.0.0.0', port=5000)
---
+++ returns.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, render_template, session, abort, flash
+import os
+import pymongo
+from datetime import datetime
+import json
+from bson.json_util import dumps, loads
+
+app = Flask(__name__)
+
+
+@app.route('/return', methods=['POST'])
+def returned():
+    mongoClient = pymongo.MongoClient('mongodb://localhost:27017/')
+    mongoDb = mongoClient['w_books']
+    bookCollection = mongoDb['books_rentals']
+
+    query = {
+        'book_id': request.form['book_id'],
+        'renter_name': request.form['renter_name']}
+    updates = {"$set": {'is_returned': 1, 'return_date': datetime.now()}}
+
+    bookCollection.update_one(query, updates)
+
+    return dumps(bookCollection.find())
+
+
+if __name__ == '__main__':
+    app.secret_key = os.urandom(12)
+    app.run(host='0.0.0.0', port=5000)
