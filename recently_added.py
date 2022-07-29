6578fc7f235720946a76102d09d90e5f472cc301
Evan Nguyen <48500302+evannguyenprog@users.noreply.github.com>
2022-07-29 16:47:14 -0400

    recentlyadded done on port 5001, trending wip
    #add trending tag or trending table
    #pull all from table
    #display

--- recently_added.py
+++ recently_added.py
@@ -1,33 +1,25 @@
-from flask import Flask
-
+from flask import Flask, request, render_template, session, abort, flash
 import json
 import os
 import pymongo
-
-# mongo_client = pymongo.MongoClient('mongodb://localhost:27017/')
-# mongo_db = mongo_client['w_books']
-
+from bson.json_util import dumps, loads
 
 app = Flask(__name__)
 
-with open("../URL HERE") as f: #replace with db get 
-    recently_added = json.load(f)
-
-@app.route("/", methods=['GET'])
-def hello():
-    return json.dumps({
-        "uri": "/",
-        "subresource_uris": {
-            "recently_added": "/recentlyAdded",
-        }
-    })
-
-
+#can add user input to set the recentlyadded date
 @app.route("/recentlyAdded", methods=['GET'])
 def recently_added_list():
-    return json.dumps(recently_added)
+    mongo_client = pymongo.MongoClient('mongodb://localhost:27017/')
+    mongo_db = mongo_client['w_books']
+    mongo_coll = mongo_db['books']
+    ret = dict()
+    cursor = mongo_coll.find({"DateAdded" : { "$gt": "2030-07-25" } }) 
+    for document in cursor:
+        print(document)
+        ret = document
+    return dumps(ret)
 
 
 if __name__ == '__main__':
     #app.secret_key = os.urandom(12)   #needed?
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
