1c03a57f5c798442e2821478628756a668bd9cac
KabirSingh114 <kabirs@umich.edu>
2022-07-31 18:01:49 -0400

    Create library.py
    See Personal Library Microservice

---
+++ library.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template, session, abort, flash
+import os
+import pymongo
+from datetime import datetime
+import json
+from bson.json_util import dumps, loads
+
+mongo_client = pymongo.MongoClient('mongodb://localhost:27017/')
+mongo_db = mongo_client['w_books']
+bookCollection = mongoDb['books_rentals']
+app = Flask(__name__)
+
+
+@app.route('/library/<renter_name>', methods=['GET'])
+def get_library(renter_name):
+    genre_data = bookCollection.find({"renter_name": renter_name})
+    return dumps(genre_data)
+
+if __name__ == '__main__':
+    app.secret_key = os.urandom(12)
+    app.run(host='0.0.0.0', port=5005)
