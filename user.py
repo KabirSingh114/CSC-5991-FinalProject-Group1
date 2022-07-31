eead5c18795096846fad135133ad8af3d650a082
Chris Bernardo <ge5536@wayne.edu>
2022-07-31 11:11:11 -0400

    Created app.py for main page and moved auth to it - also added a user.py microservice

---
+++ user.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, render_template, session, abort, flash
+import os
+import pymongo
+import json
+from bson.json_util import dumps
+import requests
+from werkzeug.utils import redirect
+
+mongo_client = pymongo.MongoClient('mongodb://localhost:27017/')
+mongo_db = mongo_client['w_books']
+mongo_users_coll = mongo_db['users']
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "Welcome!"
+
+
+@app.route('/users', methods=['GET'])
+def users_list():
+    users_data = mongo_users_coll.find()
+    return dumps(users_data)
+
+
+@app.route('/users/<username>', methods=['GET'])
+def user_info(username):
+    user_data = mongo_users_coll.find_one({"Username": username})
+    hide_password = {"Password": ""}
+    user_data.update(hide_password)
+    return dumps(user_data)
+
+if __name__ == '__main__':
+    app.secret_key = os.urandom(12)
+    app.run(host='0.0.0.0', port=5002)
