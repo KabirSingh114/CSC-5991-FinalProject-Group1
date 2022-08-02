0a2439868f284f5e31a2ebe1a13e733b6c6e1bd2
KabirSingh114 <kabirs@umich.edu>
2022-08-02 01:19:19 -0400

    add ratings

---
+++ dockerpull.sh
@@ -0,0 +1,8 @@
+docker pull kabirsingh114/csc5991group1:library
+docker pull kabirsingh114/csc5991group1:rentals
+docker pull kabirsingh114/csc5991group1:users
+docker pull kabirsingh114/csc5991group1:trending
+docker pull kabirsingh114/csc5991group1:login
+docker pull kabirsingh114/csc5991group1:returns
+docker pull kabirsingh114/csc5991group1:recentlyAdded
+docker pull kabirsingh114/csc5991group1:books
